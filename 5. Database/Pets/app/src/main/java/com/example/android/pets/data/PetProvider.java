60efec73db5799504a0ea2e924f14f59da2681e8
Khushboo <112khushimeh@gmail.com>
2020-12-26 23:58:38 +0530

    database

--- 5. Database/Pets/app/src/main/java/com/example/android/pets/data/PetProvider.java
+++ 5. Database/Pets/app/src/main/java/com/example/android/pets/data/PetProvider.java
@@ -1,8 +1,11 @@
 package com.example.android.pets.data;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -11,6 +14,18 @@ public class PetProvider extends ContentProvider {
     /**  Database helper object  */
     private PetdbHelper mDbHelper;
 
+    // Uri matcher code for the content URI for the pets table
+    private static final int PETS = 100;
+
+    // Uri matcher code for the content URI for a single pet in the pets table
+    private static final int PETS_ID = 101;
+
+    private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+            static {
+        sUriMatcher.addURI(PetContract.CONTENT_AUTHORITY, PetContract.PATH_PETS, PETS);
+        sUriMatcher.addURI(PetContract.CONTENT_AUTHORITY, PetContract.PATH_PETS+"/#", PETS_ID);
+            }
     @Override
     public boolean onCreate() {
         mDbHelper = new PetdbHelper(getContext());
@@ -19,7 +34,23 @@ public class PetProvider extends ContentProvider {
 
     @Override
     public Cursor query(Uri uri, String[] strings, String s, String[] strings1, String s1) {
-        return null;
+        SQLiteDatabase database = mDbHelper.getReadableDatabase();
+
+        Cursor cursor = null;
+
+        int match = sUriMatcher.match(uri);
+        switch (match) {
+            case PETS:
+                break;
+            case PETS_ID:
+                s = PetContract.PetEntry._ID + "=?";
+                strings1 = new String[] { String.valueOf(ContentUris.parseId(uri))};
+                cursor = database.query(PetContract.PetEntry.TABLE_NAME, strings, s, strings,  null, null, s1);
+                break;
+            default:
+                throw new IllegalArgumentException("Cannot query known URI " + uri);
+        }
+        return cursor;
     }
 
     @Override
